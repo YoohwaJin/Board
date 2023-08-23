5fc08808138667bedfd7e4cdf2209f8e5f68305d
YoohwaJin <qudghkehdtod@naver.com>
2023-08-23 13:51:09 +0900

    create

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,12 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
+    #READ
     path('', views.index, name='index'), 
+    path('<int:id>/', views.detail, name= 'detail'),
+    #create
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+
 
 ]
